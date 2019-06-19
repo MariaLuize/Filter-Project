b54658433a984614142e33afce7a5402ae25a91c
JulianaSinai <juianasinai1@gmail.com>
2019-06-19 09:13:20 -0300

    geracao de graficos primeira equipe

--- graph_test.py
+++ graph_test.py
@@ -2,7 +2,7 @@ import librosa
 import matplotlib.pyplot as plt
 import librosa.display
 
-sig, sr = librosa.load(r'/Data/queen_of_the_night_16KHz.wav', duration=8.0)
+sig, sr = librosa.load(r'/home/julianasinai/Documentos/Filter-Project/Data/queen_of_the_night_16kHz.wav', duration=8.0)
 #ipd.Audio(sig, rate=sr)
 print ("Taxa de amostragem Sinal 1: ", sr)
 print ("Numero de amostras Sinal 1: ", sig.shape)
@@ -13,7 +13,7 @@ librosa.display.waveplot(sig, sr=sr)
 plt.title('Sinal 1')
 plt.show()
 
-sig2, sr2 = librosa.load(r'/Data/mountain_king_16kHz.wav', duration=8.0)
+sig2, sr2 = librosa.load(r'/home/julianasinai/Documentos/Filter-Project/Data/mountain_king_16kHz.wav', duration=8.0)
 #ipd.Audio(sig2, rate=sr2)
 print ("Taxa de amostragem Sinal 2: ", sr2)
 print ("Numero de amostras Sinal 2: ", sig2.shape)
@@ -24,7 +24,7 @@ librosa.display.waveplot(sig2, sr=sr2)
 plt.title('Sinal 2')
 plt.show()
 
-librosa.output.write_wav('/Data/Saida_queen_of_the_night_16KHz.wav', sig, sr)
+librosa.output.write_wav('/home/julianasinai/Documentos/Filter-Project/Data/Saida_queen_of_the_night_16KHz.wav', sig, sr)
 #ipd.Audio(sig, rate=sr)
-librosa.output.write_wav('/Data/Saida_mountain_king_16KHz.wav', sig2, sr2)
+librosa.output.write_wav('/home/julianasinai/Documentos/Filter-Project/Data/Saida_mountain_king_16KHz.wav', sig2, sr2)
 #ipd.Audio(sig2, rate=sr2)
