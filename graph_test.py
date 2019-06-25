3306b038b5cda65310dd63a9ea868273ba472822
Jean-mrs <jeanmoraes1994@hotmail.com>
2019-06-25 16:51:02 -0300

    graficos de espectro e de modulacao adicionados( ainda com erro)

--- graph_test.py
+++ graph_test.py
@@ -1,30 +1,77 @@
 import librosa
 import matplotlib.pyplot as plt
 import librosa.display
+import  numpy as np
 
-sig, sr = librosa.load(r'/home/damasceno/Documents/College/UFPA/5st Semester/Digital Signal Processing/Task/FIles/Voz01_16KHz.wav', duration=8.0)
+# Carregar Sinal de Audio .wav 1
+sig, sr = librosa.load(r'/home/jean/Documentos/Filter-Project/Data/mountain_king_16kHz.wav', duration=8.0)
 #ipd.Audio(sig, rate=sr)
 print ("Taxa de amostragem Sinal 1: ", sr)
 print ("Numero de amostras Sinal 1: ", sig.shape)
 
+Fs = 16000
+n = np.arange(0, 8, 1/Fs)
+
+# Plotar sinal de audio 1
 plt.rcParams['font.family'] = ['DejaVu Sans']
 plt.figure(figsize=(12, 4))
 librosa.display.waveplot(sig, sr=sr)
 plt.title('Sinal 1')
 plt.show()
 
-sig2, sr2 = librosa.load(r'/home/damasceno/Documents/College/UFPA/5st Semester/Digital Signal Processing/Task/FIles/Voz02_16KHz.wav', duration=8.0)
+# Carregar Sinal de Audio .wav 2
+sig2, sr2 = librosa.load(r'/home/jean/Documentos/Filter-Project/Data/mountain_king_16kHz.wav', duration=8.0)
 #ipd.Audio(sig2, rate=sr2)
 print ("Taxa de amostragem Sinal 2: ", sr2)
 print ("Numero de amostras Sinal 2: ", sig2.shape)
 
+# Plotar sinal de audio 2
 plt.rcParams['font.family'] = ['DejaVu Sans']
 plt.figure(figsize=(12, 4))
 librosa.display.waveplot(sig2, sr=sr2)
 plt.title('Sinal 2')
 plt.show()
 
-librosa.output.write_wav('/home/damasceno/Documents/College/UFPA/5st Semester/Digital Signal Processing/Task/FIles/Saida_Voz01_16KHz.wav', sig, sr)
-#ipd.Audio(sig, rate=sr)
-librosa.output.write_wav('/home/damasceno/Documents/College/UFPA/5st Semester/Digital Signal Processing/Task/FIles/Saida_Voz02_16KHz.wav', sig2, sr2)
-#ipd.Audio(sig2, rate=sr2)
+Ac = 1
+c = Ac*np.cos(2*np.pi*16000*n)
+
+Fm = 2
+Am = 1
+m = Am*np.sin(2*np.pi*Fm*n)
+
+s = c * (1 + sig/Ac)
+
+plt.figure(figsize=(12, 4))
+plt.plot(n, s)
+plt.title('Modulation Case')
+plt.xlabel('Time(s)')
+plt.ylabel('Amplitude')
+plt.grid()
+plt.show()
+
+
+
+# #plt.plot(sorted(freq_interest_points), abs(frequency_response), marker='o', linestyle='-', color='red', label="Resposta Aproximada")
+# plt.plot(frequency_list, amplitude_list, label="Resposta Desejada")
+# plt.xlabel("Frequência")
+# plt.ylabel("Amplitude")
+# plt.title("Transformada")
+# plt.legend()
+# plt.grid()
+# plt.figure()
+#
+#
+# #plt.plot(sorted(freq_interest_points), [180*angle(f)/pi for f in frequency_response], marker='o', linestyle='-', color='red', label="Resposta Aproximada")
+# plt.plot(frequency_list, phase_list, label="Resposta Desejada")
+# plt.xlabel("Frequência")
+# plt.ylabel("Fase")
+# plt.title("Transformada")
+# plt.legend()
+# plt.grid()
+# plt.show()
+#
+# # Gerar arquivos de audio .wav
+# librosa.output.write_wav('/home/damasceno/Documents/College/UFPA/5st Semester/Digital Signal Processing/Task/FIles/Saida_Voz01_16KHz.wav', sig, sr)
+# #ipd.Audio(sig, rate=sr)
+# librosa.output.write_wav('/home/damasceno/Documents/College/UFPA/5st Semester/Digital Signal Processing/Task/FIles/Saida_Voz02_16KHz.wav', sig2, sr2)
+# #ipd.Audio(sig2, rate=sr2)
--- graphic.py
+++ graphic.py
@@ -3,20 +3,75 @@ import numpy as np
 import wave
 import sys
 
-
-spf = wave.open(r'C:\Users\Maria Luize\Downloads\testemono.wav')
-
-#Extract Raw Audio from Wav File
-signal = spf.readframes(-1)
-signal = np.fromstring(signal, 'Int16')
-
-
-#If Stereo
+# Extract Raw Audio from Wav File
+spf = wave.open(r'/home/jean/Documentos/Filter-Project/Data/mountain_king_16kHz.wav', mode='rb')
+# If Stereo
 if spf.getnchannels() == 2:
-    print ('Just mono files')
+    print('Just mono files')
     sys.exit(0)
 
-plt.figure(1)
+Fs = 16000
+n = np.arange(0, 8, 1/Fs)
+
+sz = 44100  # Read and process 1 second at a time.
+signal = np.fromstring(spf.readframes(128000), dtype=np.int16)
+left, right = signal[0::2], signal[1::2]
+print(len(signal))
+# discrete fourier transform
+lf, rf = abs(np.fft.rfft(left)), abs(np.fft.rfft(right))
+
+plt.figure(figsize=(12, 4))
 plt.title('Signal Wave...')
-plt.plot(signal)
+plt.plot(n, signal)
+plt.show()
+
+# # plot time signal:
+# axes[0, 0].set_title("Signal")
+# axes[0, 0].plot(t, spf, color='C0')
+# axes[0, 0].set_xlabel("Time")
+# axes[0, 0].set_ylabel("Amplitude")
+
+# plot different spectrum types:
+plt.figure(figsize=(12, 4))
+plt.title("Magnitude Spectrum")
+plt.magnitude_spectrum(signal, Fs=Fs, color='C1')
+plt.tight_layout()
+plt.show()
+
+plt.figure(figsize=(12, 4))
+plt.title("Phase Spectrum ")
+plt.phase_spectrum(signal, Fs=Fs, color='C2')
+plt.tight_layout()
+plt.show()
+
+# freq = 13
+# x = np.arange(0, 1, 1.0/Fs)
+# y = np.sin(2 * np.pi * freq * x + (np.pi/2)) * np.cos(np.pi * x + (np.pi/2))
+# np.cos(6.28*fc*t)
+# plt.plot(x, y)
+# plt.show()
+
+#Fc = 50
+Ac = 1
+c = Ac*np.cos(2*np.pi*0.5*n)
+
+
+plt.figure(figsize=(12, 4))
+plt.title('Signal Wave...')
+plt.plot(n, c)
+plt.show()
+
+
+#Fm = 2
+#Am = 1
+#m = Am*np.sin(2*np.pi*Fm*n)
+
+s = c * (1 + signal/Ac)
+
+plt.figure(figsize=(12, 4))
+plt.plot(n, s)
+plt.title('Modulation Case')
+plt.xlabel('Time(s)')
+plt.ylabel('Amplitude')
+plt.grid()
 plt.show()
